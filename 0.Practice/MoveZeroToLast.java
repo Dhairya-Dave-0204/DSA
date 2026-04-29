784eaac782d20e3a4bc46cc462898d121d41046f
Dhairya <dhairyadave962@gmail.com>
2026-04-29 11:21:01 +0530

    Program to move zeros to last

---
+++ 0.Practice/MoveZeroToLast.java
@@ -0,0 +1,26 @@
+public class MoveZeroToLast {
+    public static void main(String[] args) {
+        int arr[] = { 1, 0, 3, 4, 0 };
+
+        moveToLast(arr);
+    }
+
+    public static void moveToLast(int arr[]) {
+        int pointer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[pointer];
+                arr[pointer] = temp;
+
+                pointer++;
+            }
+        }
+
+        System.out.println("The array after processing");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+}
