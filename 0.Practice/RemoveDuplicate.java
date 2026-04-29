b2eb3a57568c704329d0eb4302fba06f52e8a30e
Dhairya <dhairyadave962@gmail.com>
2026-04-29 15:35:36 +0530

    Program to move duplicates to the end of array

---
+++ 0.Practice/RemoveDuplicate.java
@@ -0,0 +1,38 @@
+// Tow pointer implementation 
+// Removing or finding the duplicate elements in an array
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 2, 2, 3 };
+
+        remove(arr);
+    }
+
+    public static void remove(int arr[]) {
+        int pointer = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[pointer - 1]) {
+                int temp = arr[i];
+                arr[i] = arr[pointer];
+                arr[pointer] = temp;
+
+                pointer++;
+            }
+        } 
+
+        System.out.println("The array after processing");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+
+        // Brute force code to find duplicate in an unsorted array
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = i + 1; j < arr.length; j++) {
+        // if (arr[j] == arr[i]) {
+        // System.out.println("The duplicate element is : " + arr[i]);
+        // }
+        // }
+        // }
+    }
+}
